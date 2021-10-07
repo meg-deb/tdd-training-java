a337651f6409f2e8af0e22ba5727a63e25ed0ca9
meg-deb <magdalena.debowicz@thoughtworks.com>
2021-10-07 18:12:07 +0200

    Removes commented code from fizzbuzz

--- fizz-buzz/src/main/java/FizzBuzz.java
+++ fizz-buzz/src/main/java/FizzBuzz.java
@@ -1,23 +1,9 @@
-import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.List;
 
 public class FizzBuzz {
 
     public List<String> fizzBuzz() {
-//        boolean isMultipleOf15 = i % 15 == 0;
-//        boolean isMultipleOf3 = i % 3 == 0;
-//        boolean isMultipleOf5 = i % 5 == 0;
-//        if(isMultipleOf15){
-//            return "FizzBuzz";
-//        }
-//        else if(isMultipleOf3){
-//                return "Fizz";
-//            }
-//        else if(isMultipleOf5){
-//                return "Buzz";
-//            }
-//        return String.valueOf(i);
         List<String> result = new java.util.ArrayList<>();
 
         for(int x = 0; x < 100; x++){
@@ -40,4 +26,3 @@ public class FizzBuzz {
         return result;
     }
 }
-//could have 3 conditions in ternary but not sure if clean?
