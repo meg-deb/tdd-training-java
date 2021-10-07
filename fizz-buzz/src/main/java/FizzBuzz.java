dbb00edc79641fa8611c4db86847747e128722eb
meg-deb <magdalena.debowicz@thoughtworks.com>
2021-10-07 18:06:10 +0200

    Returns fizz when multiple of 3

--- fizz-buzz/src/main/java/FizzBuzz.java
+++ fizz-buzz/src/main/java/FizzBuzz.java
@@ -1,18 +1,37 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FizzBuzz {
 
-    public String fizzBuzz(int i) {
-        boolean isMultipleOf15 = i % 15 == 0;
-        boolean isMultipleOf3 = i % 3 == 0;
-        boolean isMultipleOf5 = i % 5 == 0;
-        if(isMultipleOf15){
-            return "FizzBuzz";
+    public List<String> fizzBuzz() {
+//        boolean isMultipleOf15 = i % 15 == 0;
+//        boolean isMultipleOf3 = i % 3 == 0;
+//        boolean isMultipleOf5 = i % 5 == 0;
+//        if(isMultipleOf15){
+//            return "FizzBuzz";
+//        }
+//        else if(isMultipleOf3){
+//                return "Fizz";
+//            }
+//        else if(isMultipleOf5){
+//                return "Buzz";
+//            }
+//        return String.valueOf(i);
+        List<String> result = new java.util.ArrayList<>();
+
+        for(int x = 0; x < 100; x++){
+            int number = x + 1;
+            String stringNumber = String.valueOf(number);
+            if(number % 3 == 0){
+                result.add("Fizz");
+            }
+            else {
+                result.add(stringNumber);
+            }
         }
-        else if(isMultipleOf3){
-                return "Fizz";
-            }
-        else if(isMultipleOf5){
-                return "Buzz";
-            }
-        return String.valueOf(i);
+
+        return result;
     }
 }
+//could have 3 conditions in ternary but not sure if clean?
--- fizz-buzz/src/test/java/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -7,38 +11,37 @@ public class FizzBuzzTest {
 	FizzBuzz testee = new FizzBuzz();
 
 	@Test
-	public void returnsNumberIfNotMultipleOf3() {
-		String result = testee.fizzBuzz(2);
-		assertEquals("2",result);
-		String result2 = testee.fizzBuzz(7);
-		assertEquals("7",result2);
+	public void returnsList1to100() {
+
+		List<String> expectedResult = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20");
+
+		List<String> actual = testee.fizzBuzz().subList(0,20);
+		assertEquals( expectedResult , actual);
 	}
 
 	@Test
 	public void returnsFizzWhenMultipleOf3() {
-		String result = testee.fizzBuzz(3);
-		assertEquals("Fizz",result);
+		List<String> expectedResult = Arrays.asList("1", "2", "Fizz", "4", "5", "Fizz", "7", "8", "Fizz", "10", "11", "Fizz", "13", "14", "Fizz", "16", "17", "Fizz", "19", "20");
 
-		String result2 = testee.fizzBuzz(9);
-		assertEquals("Fizz",result2);
+		List<String> actual = testee.fizzBuzz().subList(0,20);
+		assertEquals( expectedResult , actual);
 	}
 
-	@Test
-	public void returnsBuzzWhenMultipleOf5() {
-		String result = testee.fizzBuzz(5);
-		assertEquals("Buzz",result);
-
-		String result2 = testee.fizzBuzz(10);
-		assertEquals("Buzz",result2);
-	}
-
-	@Test
-	public void returnsFizzBuzzWhenMultipleOf15() {
-		String result = testee.fizzBuzz(15);
-		assertEquals("FizzBuzz",result);
-
-		String result2 = testee.fizzBuzz(30);
-		assertEquals("FizzBuzz",result2);
-	}
+//	@Test
+//	public void returnsFizzWhenMultipleOf3AndBuzzWhenMultipleOf5() {
+//		List<String> expectedResult = Arrays.asList("1", "2", "Fizz", "4", "5", "Fizz", "7", "8", "Fizz", "10", "11", "Fizz", "13", "14", "Fizz", "16", "17", "Fizz", "19", "20");
+//
+//		List<String> actual = testee.fizzBuzz().subList(0,20);
+//		assertEquals( expectedResult , actual);
+//	}
+//
+//	@Test
+//	public void returnsFizzBuzzWhenMultipleOf15() {
+//		String result = testee.fizzBuzz(15);
+//		assertEquals("FizzBuzz",result);
+//
+//		String result2 = testee.fizzBuzz(30);
+//		assertEquals("FizzBuzz",result2);
+//	}
 
 }
