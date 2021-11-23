354974dc5595319bfad92dc4133ff30f1dfaf6d4
meg-deb <magdalena.debowicz@thoughtworks.com>
2021-11-23 19:49:57 +0100

    Adds message logging to user

--- coffee-machine/src/main/java/DrinkMaker.java
+++ coffee-machine/src/main/java/DrinkMaker.java
@@ -1,43 +1,38 @@
 public class DrinkMaker {
     CoffeeMachineUI coffeeMachineUI;
 
+    public DrinkMaker(CoffeeMachineUI coffeeMachineUI){
+        this.coffeeMachineUI = coffeeMachineUI;
+    }
+
     public Drink make(String command) {
-        String drinkType = null;
-        Integer sugar = 0;
+        int sugar;
         boolean stick;
 
-        char[] charsInCommand = command.toCharArray();
+        String[] order = command.split(":");
 
-        if(charsInCommand[0] == 'M'){
-            coffeeMachineUI.printMessage(command);
-        }
-        else if(charsInCommand[0] == 'H'){
-            drinkType = "Hot Chocolate";
-        }
-        else if(charsInCommand[0] == 'T'){
-            drinkType = "Tea";
-        }
-        else{
-            drinkType = "Coffee";
+        if(order[0].equals("M")){
+            coffeeMachineUI.printMessage(command.substring(2));
+            return null;
         }
 
-        if(charsInCommand[2] != ':'){
-            sugar = Integer.parseInt(String.valueOf(charsInCommand[2]));
+        if(order.length > 1){
+            sugar = Integer.parseInt(String.valueOf(order[1]));
             stick = true;
         }
         else{
             stick = false;
+            sugar = 0;
         }
 
-        if (drinkType == "Hot Chocolate") {
+        if (order[0].equals("H")) {
             return new HotChocolate(sugar, stick);
         }
-        else if (drinkType == "Coffee") {
+        else if (order[0].equals("C")) {
             return new Coffee(sugar, stick);
         }
         else{
             return new Tea(sugar, stick);
         }
-
     }
 }
--- coffee-machine/src/test/java/DrinkMakerTest.java
+++ coffee-machine/src/test/java/DrinkMakerTest.java
@@ -1,48 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DrinkMakerTest {
+    CoffeeMachineUI mockedCoffeeMachineUI;
+    DrinkMaker testDrinkMaker;
+
+    @BeforeEach
+    private void beforeEach() {
+        mockedCoffeeMachineUI = Mockito.mock(CoffeeMachineUI.class);
+        testDrinkMaker = new DrinkMaker(mockedCoffeeMachineUI);
+    }
 
     @Test
     public void shouldMakeHotChocolate() {
-        DrinkMaker testDrinkMaker = new DrinkMaker();
         String order = "H::";
         HotChocolate hotChocolate = new HotChocolate(0, false);
 
         Drink result = testDrinkMaker.make(order);
 
         assertThat(result).isEqualTo(hotChocolate);
-        System.out.println(hotChocolate);
     }
 
     @Test
     public void shouldMakeHotChocolateWithSugarAndStick() {
-        DrinkMaker testDrinkMaker = new DrinkMaker();
         String order = "H:1:0";
         HotChocolate hotChocolate = new HotChocolate(1, true);
 
         Drink result = testDrinkMaker.make(order);
 
         assertThat(result).isEqualTo(hotChocolate);
-        System.out.println(hotChocolate);
     }
 
     @Test
     public void shouldMakeCoffee() {
-        DrinkMaker testDrinkMaker = new DrinkMaker();
         String order = "C::";
         Coffee coffee = new Coffee(0, false);
 
         Drink result = testDrinkMaker.make(order);
 
         assertThat(result).isEqualTo(coffee);
-        System.out.println(coffee);
     }
 
     @Test
     public void shouldMakeCoffeeWith2SugarAndStick() {
-        DrinkMaker testDrinkMaker = new DrinkMaker();
         String order = "C:2:0";
         Coffee coffee = new Coffee(2, true);
 
@@ -53,25 +56,32 @@ public class DrinkMakerTest {
 
     @Test
     public void shouldMakeTea() {
-        DrinkMaker testDrinkMaker = new DrinkMaker();
         String order = "T::";
         Tea tea = new Tea(0, false);
 
         Drink result = testDrinkMaker.make(order);
 
         assertThat(result).isEqualTo(tea);
-        System.out.println(tea);
     }
 
     @Test
     public void shouldMakeTeaWith3SugarAndStick() {
-        DrinkMaker testDrinkMaker = new DrinkMaker();
         String order = "T:3:0";
         Tea tea = new Tea(3, true);
 
         Drink result = testDrinkMaker.make(order);
 
         assertThat(result).isEqualTo(tea);
-        System.out.println(tea);
+    }
+
+    @Test
+    public void shouldCallCoffeeMachineUIToPrintMessage() {
+        String order = "M:test";
+
+        Drink result = testDrinkMaker.make(order);
+
+        Mockito.verify(mockedCoffeeMachineUI).printMessage("test");
+
+        assertThat(result).isEqualTo(null);
     }
 }
