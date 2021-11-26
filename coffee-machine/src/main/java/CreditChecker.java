92b6924805b1b813454ff385813a95a35b1285e7
meg-deb <magdalena.debowicz@thoughtworks.com>
2021-11-26 21:25:44 +0100

    Back to if else in CreditChecker

--- coffee-machine/src/main/java/CreditChecker.java
+++ coffee-machine/src/main/java/CreditChecker.java
@@ -3,13 +3,12 @@ import models.DrinkName;
 public class CreditChecker {
 
     public boolean isEnoughMoneyForDrink(double money, DrinkName drinkName) {
-        switch (drinkName){
-            case TEA:
-                return money >= Tea.PRICE;
-            case HOT_CHOCOLATE:
-                return money >= HotChocolate.PRICE;
-            case COFFEE:
-                return money >= Coffee.PRICE;
+        if (drinkName.equals(DrinkName.TEA)) {
+            return money >= Tea.PRICE;
         }
+        else if (drinkName.equals(DrinkName.HOT_CHOCOLATE)) {
+            return money >= HotChocolate.PRICE;
+        }
+        return money >= Coffee.PRICE;
     }
 }
