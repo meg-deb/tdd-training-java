3db0150dec76943aa77ebef32fadf79bd6ede4ae
meg-deb <magdalena.debowicz@thoughtworks.com>
2021-11-26 22:08:57 +0100

    User credit input first iteration

--- coffee-machine/src/main/java/CoffeeMachine.java
+++ coffee-machine/src/main/java/CoffeeMachine.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
     public static void main(String[] args) {
+        Scanner scannerForInput = new Scanner(System.in);
+        System.out.println("Welcome to Coffee Machine!\n" +
+                "Available drinks:\n" +
+                "Tea - 0,4 euro,\n" +
+                "Hot chocolate - 0,5 euro,\n" +
+                "Coffee - 0,6 euro.\n\n" +
+                "Please give credit for drink:");
+
+        String userCredit = scannerForInput.nextLine();
+        System.out.println("Thank you. Your credit is: " + userCredit + "\u20ac");
+
 //        drinkToMake = user chooses what drink to do
 //
 //        isDrinMakerDoDrink? = creditChecker(money, drinkToMake)
